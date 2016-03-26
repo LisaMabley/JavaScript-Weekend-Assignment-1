49a1b95469aea456bf9f08bb7357e3bc165b54ba
LisaMabley <lisamabley@gmail.com>
2016-03-26 14:26:24 -0500

    added new claims & variables

--- app.js
+++ app.js
@@ -28,16 +28,67 @@ var claim5 = {
 	visitCost: 770
 }
 
-var initialList = [claim1, claim2, claim3, claim4, claim5]
-
+var claimList = [claim1, claim2, claim3, claim4, claim5];
 var totalPayedOut = 0;
 
-function claim(name, type, cost){
+// Add new claims
+claimList.push(new Claim('Lisa Mabley', 'Mental Health', 100));
+claimList.push(new Claim('Greg Schultz', 'Primary Care', 75));
+claimList.push(new Claim('Grace Jones', 'Specialist', 225));
+claimList.push(new Claim('Melissa Maynard', 'Primary Care', 300));
+claimList.push(new Claim('Frank Zappa', 'Emergency', 316));
+claimList.push(new Claim('Sarah Silverman', 'Emergency', 452));
+claimList.push(new Claim('Robin Williams', 'Primary Care', 560));
+claimList.push(new Claim('Henry Higgins', 'Specialist', 485));
+claimList.push(new Claim('Maryanne Marshall', 'Specialist', 225));
+
+// Process claims
+for (var i = 0; i < claimList.length; i++) {
+	setPercentCovered(claimList[i]);
+	setCoveredAmount(claimList[i]);
+}
+
+// Log result
+console.log('Company liability for all claims: $' + totalPayedOut.toLocaleString());
+
+// Constructor
+function Claim(name, type, cost){
 	this.patientName = name;
 	this.visitType = type;
 	this.visitCost = cost;
 }
 
-//function to determine percent covered
+// Determine percent covered and set claim variable
+function setPercentCovered(claim) {
+	console.log(claim);
+	switch(claim.visitType) {
+		case 'Emergency':
+			claim.percentCovered = 1;
+			break;
 
-//function to determine amount covered
+		case 'Primary Care':
+			claim.percentCovered = 0.5;
+			break;
+
+		case 'Specialist':
+			claim.percentCovered = 0.1;
+			break;
+
+		default:
+			// Covers Optical and any other uncovered providers
+			claim.percentCovered = 0.0;
+			break;
+	}
+}
+
+// Determine amount covered and set claim variable
+function setCoveredAmount(claim) {
+	if (claim.percentCovered) {
+		claim.amountCovered = parseInt(claim.visitCost * claim.percentCovered);
+		totalPayedOut += claim.amountCovered;
+		console.log('Paid out $' + claim.amountCovered.toLocaleString() + ' for ' + claim.patientName);
+
+	} else {
+		console.log('This claim is not covered. Please call customer service at 1-Not-Our-Prob to appeal this decision.');
+	}
+}
